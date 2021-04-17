0703904d1b9470b9de9492fa69445533dd3f0ba6
kenthehacker <59815209+kenthehacker@users.noreply.github.com>
2021-04-16 21:11:58 -0500

    starter code

---
+++ exampleObj.py
@@ -0,0 +1,25 @@
+#example entity code
+class alien:
+    pass
+    def __init__(self):
+        self.height = 20
+        self.width = 20
+        self.speedy = 30
+        self.speedx = 20
+        self.x=0
+        self.y = 9
+
+    def collisionDetection(self,otherObject):
+        pass
+    def getX(self):
+        return self.x
+        
+    def getY(self):
+        return self.y
+
+
+
+
+
+
+
---
+++ mainSpace.py
@@ -0,0 +1,41 @@
+import pygame
+import sys
+import os
+pygame.init()
+
+#pixel size of game
+xSizeScreen = 700
+ySizeScreen = 700
+
+# set the fps
+fps = pygame.time.Clock()
+
+#variable dumps:
+screen = pygame.display.set_mode((xSizeScreen, ySizeScreen))
+running = True #runs the game until this is false
+bg = pygame.image.load("spaceBackDrop.png")
+pressSpaceText = pygame.image.load("text.png")
+#example pressSpaceStuff = pygame.transform.scale(pressSpaceText,(width,height))
+
+def splashScreen():
+    screen.blit(bg,(0,-200))
+    screen.blit(pressSpaceText, (-135,0))
+
+
+
+while(running):
+    fps.tick(60)
+    splashScreen()
+
+
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+    pygame.display.update()
+
+pygame.quit()
+
+
+
+
+
